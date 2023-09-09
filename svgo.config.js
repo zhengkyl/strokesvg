97ef010b58a5a8ad4f5b7a53777ddd6fbdce22c8
Kyle Zheng <kylezheng73@gmail.com>
2023-09-09 00:18:32 -0400

    working svgo config

--- svgo.config.js
+++ svgo.config.js
@@ -1,7 +1,74 @@
+const path = require("node:path");
+const { querySelectorAll, querySelector } = require("svgo/lib/xast.js");
+
 module.exports = {
   js2svg: {
     indent: 2,
-    pretty: true, // boolean, false by default
+    pretty: true,
   },
-  plugins: ["preset-default"],
+  plugins: [
+    {
+      name: "preset-default",
+      params: {
+        overrides: {
+          collapseGroups: false,
+          convertPathData: {
+            floatPrecision: 0,
+          },
+        },
+      },
+    },
+    "removeDimensions", // height/width -> viewbox
+    {
+      name: "removeAttributesBySelector",
+      params: {
+        selectors: [
+          {
+            selector: "svg",
+            attributes: "xmlns:xlink",
+          },
+          {
+            selector: "clipPath",
+            attributes: "clipPathUnits",
+          },
+          {
+            selector: "use",
+            attributes: ["width", "height"],
+          },
+          {
+            selector: "g > path",
+            attributes: "style",
+          },
+        ],
+      },
+    },
+    {
+      name: "prefixIds",
+      params: {
+        delim: "",
+        prefixClassNames: false,
+        prefix: (_, info) => path.basename(info.path, ".svg"),
+      },
+    },
+    {
+      name: "pluginname",
+      fn: (root) => {
+        const nodes = querySelectorAll(root, "[xlink\\:href]");
+        for (const node of nodes) {
+          if (node.attributes["xlink:href"]) {
+            node.attributes["href"] = node.attributes["xlink:href"];
+            delete node.attributes["xlink:href"];
+          }
+        }
+
+        const shadowG = querySelector(root, "g:first-of-type");
+        shadowG.attributes.style = "fill:#ccc";
+
+        const strokeG = querySelector(root, "g:last-of-type");
+        strokeG.attributes.style = "fill:none;stroke:#000;stroke-width:128";
+
+        return {};
+      },
+    },
+  ],
 };
